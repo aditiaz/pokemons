18590dac148be5d4ea5e40043a984f4d4e99120f
aditiaz <aditiaan96a@gmail.com>
2023-08-14 09:42:12 +0700

    vercel

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import { useState } from "react";
-import "./App.css";
-import { Route, Routes, BrowserRouter } from "react-router-dom";
-import { PokemonsContextProvider } from "./context/pokemonContext";
-import { FavortiePokemonsContextProvider } from "./context/FavoritePokemonsContext";
-import Home from "./pages/home";
-import DetailPokemons from "./pages/DetailPokemons";
-import FavoritePokemons from "./pages/FavoritePokemons";
+import { useState } from 'react';
+import './App.css';
+import { Route, Routes, BrowserRouter } from 'react-router-dom';
+import { PokemonsContextProvider } from './context/pokemonContext';
+import { FavortiePokemonsContextProvider } from './context/FavoritePokemonsContext';
+import Home from './pages/home';
+import DetailPokemons from './pages/DetailPokemons';
+import FavoritePokemons from './pages/FavoritePokemons';
 function App() {
   return (
     <>
